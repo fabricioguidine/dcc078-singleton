3fd5841c961f93799b81105fa09c7a2901de7c78
fabricioguidine <fabricioguidine@gmail.com>
2023-09-17 17:31:34 -0300

    ajusta teste

--- src/test/java/SingletonTest.java
+++ src/test/java/SingletonTest.java
@@ -1,10 +1,11 @@
 import junit.framework.TestCase;
+import org.example.Singleton;
 import org.junit.Test;
 
 public class SingletonTest extends TestCase{
     @Test
     public void testSetUrl(){
-        Singleton.getInstance().setUrl();
+        Singleton.getInstance().setUrl("jdbc:localhost:8888/bd");
         assertEquals("jdbc:localhost:8888/bd", Singleton.getInstance().getUrl());
     }
 }
